35e02eda16abbf2f0d3144b8936dba8626a5b7ec
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-06-26 18:26:16 +0600

    database connected

--- src/index.server.js
+++ src/index.server.js
@@ -2,6 +2,7 @@ const  express = require("express");
 const app = express();
 const cors = require("cors");
 const env = require("dotenv");
+const mongoose = require('mongoose');
 
 // environment variable
 env.config();
@@ -9,6 +10,14 @@ env.config();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// MongoDB connection (mongoose)
+// mongodb+srv://admin:<password>@cluster0.wdnsw.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wdnsw.mongodb.net/?retryWrites=true&w=majority`)
+.then(()=>{
+    console.log("MongoDB Database Conntected.")
+});
+
 
 // GET API
 app.get('/', (req, res, next)=>{
