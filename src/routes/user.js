cd0df773abc3f7eb90c548ed253b2764a30f7156
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-06-27 10:23:45 +0600

    signup via controller done

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,47 +1,13 @@
 const  express = require("express");
+const { signup } = require("../controller/user");
 const router = express.Router();
-const User = require('../models/user');
+
+
+router.post('/signup', signup);
 
 router.post('/signin', (req, res, next)=>{
+
     
-
-});
-
-router.post('/signup', (req, res, next)=>{
-
-    User.findOne({ email: req.body.email})
-    .exec((error, user)=>{
-        if(user) return res.status(400).json({
-            message: 'User already registered.'
-        });
-        
-        const {
-            firstName,
-            lastName,
-            email,
-            password
-        } = req.body;
-        const _user = new User({
-            firstName,
-            lastName,
-            email,
-            password,
-            username: Math.random().toString()
-        });
-
-        _user.save((error, data)=>{
-            if(error){
-                return res.status(400).json({
-                    message: 'Something wrong happend !'
-                })
-            }
-            if(data){
-                return res.status(201).json({
-                    message: "User created successfully !"
-                })
-            }
-        })
-    })
 });
 
 module.exports = router;
